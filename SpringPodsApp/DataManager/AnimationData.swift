bbe48aead0e3e5fe5cd8ab1a2a1dec8001090fb5
Alexey Barinov <begoodbebad2012@gmail.com>
2021-04-28 19:27:26 +0300

    init

---
+++ SpringPodsApp/DataManager/AnimationData.swift
@@ -0,0 +1,32 @@
+//
+//  AnimationData.swift
+//  SpringPodsApp
+//
+//  Created by Mac on 28.04.2021.
+//
+
+
+class TypeAnimation {
+    
+    static let shared = TypeAnimation()
+    
+    let animationType = ["slideLeft", "slideRight", "slideDown",
+                         "slideUp","squeezeLeft", "squeezeRight",
+                         "squeezeDown","squeezeUp", "fadeIn",
+                         "fadeOut", "fadeOutIn","fadeInLeft",
+                         "fadeInRight", "fadeInDown", "fadeInUp",
+                         "zoomIn", "zoomOut", "fall", "shake",
+                         "pop", "flipX","flipY", "morph",
+                         "squeeze", "flash", "wobble", "swing"]
+    
+    let curveType = ["easeIn", "easeOut", "easeInOut", "linear",
+                     "spring","easeInSine", "easeOutSine",
+                     "easeInOutSine", "easeInQuad","easeOutQuad",
+                     "easeInOutQuad", "easeInCubic","easeOutCubic",
+                     "easeInOutCubic", "easeInQuart","easeOutQuart",
+                     "easeInOutQuart", "easeInQuint","easeOutQuint",
+                     "easeInOutQuint", "easeInExpo","easeOutExpo",
+                     "easeInOutExpo", "easeInCirc","easeOutCirc",
+                     "easeInOutCirc", "easeInBack","easeOutBack",
+                     "easeInOutBack"]
+}
